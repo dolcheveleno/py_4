307a42d27ceec92bb6985d95e7d346c8ac44b44c
Olya <dolcheveleno@mail.ru>
2022-10-06 13:32:34 +0300

    2

--- 2.py
+++ 2.py
@@ -1,25 +1,19 @@
-# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-# Пример:
-# - [2, 3, 4, 5, 6] => [12, 15, 16];
-# - [2, 3, 5, 6] => [12, 15]
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
-from audioop import mul
-import random
+userEnter = int(input("Введите натуральное число N: "))
 
+def Factor(y):                               
+    list = []
+    x = 2
+    while x * x <= y:
+        if y % x == 0:
+            list.append(x)
+            y //= x
+        else:
+            x += 1
+    if y > 1:
+        list.append(y)
+    return list
 
-n = int(input('Введите количество элементов списка: '))
-
-list = []
-
-for i in range(0, n):
-    list.append(random.randint(1,5))
-
-print(list)
-
-newList = []
-for i in range(0, int((len(list) + 1) / 2)):
-    multi = list[i] * list[len(list)-i-1]
-    newList.append(multi)
-
-print('Произведение пар чисел списка, где парой считаем первый и последний элемент: ')
-print(newList)
+print(f"Список простых множителей числа {userEnter} равен: ")
+print(Factor(userEnter))
