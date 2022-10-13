2eee0f8558b7c559d1ec35dfbabbab855da51016
Olya <dolcheveleno@mail.ru>
2022-10-13 13:33:05 +0300

    1

--- 1.py
+++ 1.py
@@ -1,16 +1,18 @@
-# Вычислить число c заданной точностью d
-# Пример:
-# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-d = input('Введите дробное число в диапазоне от 0.1 до 0.0000000001: ')
-if 0.1 >= float(d) >= 0.0000000001:
-    n = abs(d.find('.') - len(d))-1
-    h = int(n)
-    k = 1
-    x = 0
-    import random
-    for k in range(1, 1000000):
-        x = x+4*((-1)**(k+1))/(2*k-1)
-    print(f"Число pi с заданной точностью *{d}*: {round(x,h)}")
-else:
-    print("Ошибка ввода, введите дробное число в диапазоне от 0.1 до 0.0000000001!")
+import os
+os.system('cls')
+
+from hashlib import new
+from ntpath import join
+
+line = 'олрабв читсм абвопьп мгнеп попабвщршр мтпо'
+print(line)
+str = line.split() 
+print(str)
+for elem in str:
+    if elem.find('абв') != -1:
+        str.remove(elem) 
+print(str)            
+newline = ' '.join(str) 
+print(newline)    
