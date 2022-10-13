f41bb9848b18de92242f4360bbd21c925873f4bd
Olya <dolcheveleno@mail.ru>
2022-10-13 13:39:21 +0300

    3

--- 3.py
+++ 3.py
@@ -1,34 +1,63 @@
-# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+# Создайте программу для игры в ""Крестики-нолики"".
 
-import os
-os.system('cls')
 import random
+import os
 
-n = int(input('Введите число =  '))
-mas = []
-for i in range(n*4):
-   mas.append(random.randint(0, 20))
-print('Это произвольный список =', mas)
-print()
-mas.sort()
-print('Это сортированный список =', mas)
-print()
+def print_array(field):  
+    os.system('cls')
+    for i in range(1, 10):
+        print(f"{field[i - 1]} ", end="")
+        if not i%3: print()
 
-list = [1]
-list[0] = mas[0]
+def filling(array, array_ex, set): 
+    x = int(input("Выберете клетку: "))
+    if x in array_ex:
+        for i in range(len(array)):
+            if array[i] == x:
+                if set: array[i] = "X"
+                else: array[i] = "O"
+        array_ex.remove(x)
+    else:
+        print("Поле занято, или вы вышли за границы игрового поля")
+        filling(array, array_ex, set)
 
-x = int(list[0])
-temp = 0
 
-for i in range(len(mas)):
-    temp = int(mas[i])
-  
-    if x == temp:
-        x = temp
-        i = i + 1
+def win_set(array): 
+    if (array[0] == array[1] == array[2] or
+     array[3] == array[4] == array[5] or
+     array[6] == array[7] == array[8] or
+     array[0] == array[3] == array[6] or
+     array[1] == array[4] == array[7] or
+     array[2] == array[5] == array[8] or
+     array[0] == array[4] == array[8] or
+     array[2] == array[4] == array[6]) :
+        return True
+    else:
+        return False
 
-    if x < temp:
-        list.append(temp)
-        x = temp
-        i = i + 1
-print('Это неповторяющийся список',list)
+
+def play(array, array_ex, set):  
+    print_array(array)
+    if array_ex == []:
+        print("Победила Дружба!")
+        return
+
+    if set:
+        player = "1-й"
+    else: 
+        player = '2-й'
+    print(f"Ходит {player} игрок")
+
+    filling(array, array_ex, set)
+
+    if win_set(array):
+        print_array(array)
+        print(f"Выиграл {player} игрок \n GAME OVER")
+        return
+    play(array, array_ex, not set)
+
+field = [int(i) for i in range(1, 10)]      
+field_ex = [int(i) for i in range(1, 10)] 
+
+set = random.randint(0,1)
+play(field, field_ex, set)
